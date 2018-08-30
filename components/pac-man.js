34864b630dae1467b064d916b156a374074ad674
Steve Lewis <slewis@kajabi.com>
2018-08-30 15:21:58 -0700

    Logic for teleportation tunnels

--- components/pac-man.js
+++ components/pac-man.js
@@ -1,10 +1,22 @@
 AFRAME.registerComponent('pac-man', {
 
   init: function() {
-    console.log("registered as pac-man")
     this.registerEventListeners = this.registerEventListeners.bind(this);
 
     this.registerEventListeners();
+  },
+
+  tick: function() {
+    var currentPosition = this.el.getAttribute('position')
+
+    if(currentPosition.z <= -25){
+      // Teleport the user to 27
+      this.el.setAttribute('position', { x: currentPosition.x, y: currentPosition.y, z: 27 })
+    }
+
+    if(currentPosition.z >= 28){
+      this.el.setAttribute('position', { x: currentPosition.x, y: currentPosition.y, z: -24 })
+    }
   },
 
   registerEventListeners: function() {
