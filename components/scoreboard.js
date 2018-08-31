7a76403df2d21cd70078bcfdd91b545f6ac47a49
Steve Lewis <slewis@kajabi.com>
2018-08-30 17:02:21 -0700

    Add some dots, and send pacman to the center when he clears

--- components/scoreboard.js
+++ components/scoreboard.js
@@ -4,6 +4,7 @@ AFRAME.registerComponent('scoreboard', {
     this.registerEventListeners = this.registerEventListeners.bind(this);
 
     this.registerEventListeners();
+    this.resetDots()
   },
 
   registerEventListeners: function() {
@@ -20,11 +21,11 @@ AFRAME.registerComponent('scoreboard', {
   },
 
   boardClear: function(e){
-    var player = document.querySelector('a-entity[pac-man]').parentNode
+    var player = document.querySelector('a-entity[pac-man]')
     console.log(player)
-    player.object3D.position.set(0, 0, 0)
+    player.object3D.position.set(-23.9, 0, 0.739)
 
-    //this.resetDots() // Restore all dots.
+    this.resetDots() // Restore all dots.
   },
 
   resetDots: function() {
@@ -35,9 +36,33 @@ AFRAME.registerComponent('scoreboard', {
     }
 
     var positions = [
-      {x: 0, y: 1, z: -5},
+      {x: 0, y: 1, z: 26},
+      {x: 0, y: 1, z: 24},
+      {x: 0, y: 1, z: 22},
+      {x: 0, y: 1, z: 20},
+      {x: 0, y: 1, z: 18},
+      {x: 0, y: 1, z: 16},
+      {x: 0, y: 1, z: 14},
+      {x: 0, y: 1, z: 12},
+      {x: 0, y: 1, z: 10},
+      {x: 0, y: 1, z: 8},
+      {x: 0, y: 1, z: 6},
+      {x: 0, y: 1, z: 4},
+      {x: 0, y: 1, z: 2},
+      {x: 0, y: 1, z: 0},
+      {x: 0, y: 1, z: -2},
+      {x: 0, y: 1, z: -4},
+      {x: 0, y: 1, z: -6},
+      {x: 0, y: 1, z: -8},
       {x: 0, y: 1, z: -10},
-      {x: 0, y: 1, z: -15},
+      {x: 0, y: 1, z: -12},
+      {x: 0, y: 1, z: -14},
+      {x: 0, y: 1, z: -16},
+      {x: 0, y: 1, z: -18},
+      {x: 0, y: 1, z: -20},
+      {x: 0, y: 1, z: -22},
+      {x: 0, y: 1, z: -24},
+
     ]
 
     for(i=0; i < positions.length; i++){
